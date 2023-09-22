8b7a55e898b4083a42b2e20f246865fb76276aa1
Elias Bakhshi <bakhshielias@gmail.com>
2023-09-22 12:43:07 +0200

    Fixed some issues and readjusted some lines

--- functions/main.py
+++ functions/main.py
@@ -2,9 +2,12 @@
 functions the application
  """
 
-import time, random, math
+import time
+import random
+import math
 
-def clearConsole():
+def clear_console():
+    """ Clear the console """
     print(chr(27) + "[2J" + chr(27) + "[;H")
 
 
@@ -17,16 +20,17 @@ def sanitize_str(word, characters_to_remove = None):
 
 def get_lines(file_name):
     """ Get text file's content line by line """
-    with open(file_name) as f_easy:
+    with open(file_name, encoding="utf-8") as f_easy:
         return f_easy.readlines()
 
 def get_text(file_name):
     """ Get entire text file's content """
-    with open(file_name) as f_easy:
+    with open(file_name, encoding="utf-8") as f_easy:
         return f_easy.read()
 
 
 def get_animal_name(score = 0):
+    """ Get the an animal name according to the score """
     if 10 <= score < 20:
         return "Sengångare"
     elif 20 <= score < 30:
@@ -51,26 +55,27 @@ def get_animal_name(score = 0):
         return "Pilgrimsfalk"
     return None
 
-def show_lines(lines):
+def start_practice(lines):
     """ Show lines and get info """
     fails = {}
     start_time = 0
     finish_time = 0
     count_written_words = 0
+    count_extra_word = count_failed_words = count_total_words = count_total_character= 0
+    start_time = time.time()
     for line in lines:
-        count_extra_word = count_failed_words = count_total_words = count_total_character= 0
         line = line.strip()
-        clearConsole()
+        clear_console()
         print(line)
         # Start timer
-        start_time = time.time()
         the_input = input("")
         count_total_words += int(len(sanitize_str(line,".,-()").split(" ")))
         count_total_character += int(len(sanitize_str(the_input,".,-() \n")))
         line = sanitize_str(str(line), ".,-()").strip().split(" ")
         the_input = sanitize_str(str(the_input), ".,-()").strip().split(" ")
         #if the written line has more words
-        if len(the_input) > len(line): count_extra_word = len(the_input) - len(line)
+        if len(the_input) > len(line):
+            count_extra_word = len(the_input) - len(line)
         count_written_words += len(the_input)
         #if the written line has more words add extra empty parameters to the list
         if len(line) - len(the_input):
@@ -87,8 +92,8 @@ def show_lines(lines):
 
     finish_time = time.time()
     duration_sec =  math.ceil((finish_time - start_time))
-    gross_WPM = (count_written_words * 60) / duration_sec
-    net_WPM = gross_WPM - ((count_failed_words * 60) / duration_sec)
+    gross_wpm = (count_written_words * 60) / duration_sec
+    net_wpm = gross_wpm - ((count_failed_words * 60) / duration_sec)
     return {
         "failed_letters": sorted(fails.items(), key=lambda item: item[1], reverse=True),
         "count_extra_word": count_extra_word,
@@ -96,28 +101,43 @@ def show_lines(lines):
         "count_total_words": count_total_words,
         "count_total_character": count_total_character,
         "duration": str(round(duration_sec, 1)) + "s",
-        "gross_WPM": gross_WPM,
-        "net_WPM": net_WPM,
-        "category": get_animal_name(net_WPM)
+        "gross_wpm": gross_wpm,
+        "net_wpm": net_wpm,
+        "category": get_animal_name(net_wpm)
     }
+
+def display_result(result):
+    """ Display result information in the console. """
+    clear_console()
+    present_fail = 100 - ((((result['count_total_words'] -
+                             result['count_failed_words'] - result['count_extra_word'] ) /
+                             result['count_total_words']) * 100))
+    print("Feltecken:")
+    print(result['failed_letters'])
+    print("Precentuellt fel: " + str(present_fail) + "%")
+    print("Total tid: ", str(result['duration']))
+    print("Gross WPM: ", result['gross_wpm'])
+    print("Net WPM: ", result['net_wpm'])
 
 def save_result(file_name, result):
     """ Save the result in the score.txt """
-    with open(file_name, "a+") as f_score:
+    with open(file_name, "a+", encoding="UTF-8") as f_score:
         # Check if the last line has next line character or not
         f_score.seek(0)
         the_content = f_score.read()
-        last_char = the_content[-1]
-        next_line = "\n"
-        if last_char == "\n": next_line = ""
-        f_score.write(f"{next_line}{result['name']}|{result['net_WPM']}|{result['level']}")
+        next_line = ""
+        if len(the_content):
+            last_char = the_content[-1]
+            if last_char != "\n":
+                next_line = "\n"
+        f_score.write(f"{next_line}{result['name']}|{result['net_wpm']}|{result['level']}")
         return True
 
 def show_result(file_name):
     """ Show the result from the score.txt """
     levels = ["easy", "medium", "hard"]
     levels.sort(reverse=True)
-    with open(file_name) as f_score:
+    with open(file_name, encoding="UTF-8") as f_score:
         the_lines = f_score.readlines()
         the_list = []
         for the_line in the_lines:
@@ -128,7 +148,7 @@ def show_result(file_name):
         the_list.sort(key=lambda row: row[2], reverse=True)
         for the_line in the_list:
             # Print line in the format
-            print("{: <20} {: <20} {: <20}".format(*the_line))
+            print(f"{the_line[0]: <20} {the_line[1]: <20} {the_line[2]: <20}")
 
 def get_random_char():
     """ Generate a random character. """
@@ -144,7 +164,7 @@ def practice_random_char():
     fails = {}
 
     while duration_sec > time.time() - start_time :
-        clearConsole()
+        clear_console()
         random_char = str(get_random_char()).strip()
         print(random_char)
         the_input = input("").strip()
@@ -153,13 +173,13 @@ def practice_random_char():
             count_failed_character += 1
             fails[random_char] = fails.get(random_char, 0) + 1
 
-    gross_WPM = (count_total_character * 60) / duration_sec
-    net_WPM = gross_WPM - ((count_failed_character * 60) / duration_sec)
+    gross_wpm = (count_total_character * 60) / duration_sec
+    net_wpm = gross_wpm - ((count_failed_character * 60) / duration_sec)
 
     return {
         "failed_letters": sorted(fails.items(), key=lambda item: item[1], reverse=True),
         "count_failed_character": count_failed_character,
         "count_total_character": count_total_character,
-        "gross_WPM": gross_WPM,
-        "net_WPM": net_WPM
+        "gross_wpm": gross_wpm,
+        "net_wpm": net_wpm
     }
--- main.py
+++ main.py
@@ -2,15 +2,14 @@
 This the main file for the application
 """
 
-from functions.main import get_lines, get_text, show_lines, clearConsole, save_result, show_result, practice_random_char
-
+import functions.main as mf
 
 def main():
     """ Script starts from here """
     is_running = True
     while is_running:
         # The menu
-        clearConsole()
+        mf.clear_console()
         print("--- <<< The menu >>> ---")
         print("\n")
         print("1:  Easy.")
@@ -26,58 +25,58 @@ def main():
         if u_choice == "q":
             is_running = False
         elif u_choice == "1":
-            the_lines = get_lines("typing/easy copy.txt")
-            result = show_lines(the_lines)
+            the_lines = mf.get_lines("typing/easy.txt")
+            result = mf.start_practice(the_lines)
             input("Press enter to see the result ...")
-            clearConsole()
-            present_fail = 100 - (((result['count_total_words'] - result['count_failed_words'] - result['count_extra_word'] ) / result['count_total_words']) * 100)
-            print("Feltecken:")
-            print(result['failed_letters'])
-            print("Precentuellt fel: " + str(present_fail) + "%")
-            print("Total tid: ", str(result['duration']))
-            print("Gross WPM: ", result['gross_WPM'])
-            print("Net WPM: ", result['net_WPM'])
+            mf.display_result(result)
             result["name"] = str(input("\nEnter username to add to high score: "))
             result["level"] = "easy"
-            if save_result("typing/score.txt", result) : print("The information has been saved")
+            if mf.save_result("typing/score.txt", result) :
+                print("The information has been saved")
             else: print("Something went wrong!")
 
-
-
-
         elif u_choice == "2":
-            print(get_lines("typing/medium.txt"))
-            result = show_lines(the_lines)
+            the_lines = mf.get_lines("typing/medium.txt")
+            result = mf.start_practice(the_lines)
             input("Press enter to see the result ...")
-            print("Feltecken:")
-            print(result)
+            mf.display_result(result)
+            result["name"] = str(input("\nEnter username to add to high score: "))
+            result["level"] = "medium"
+            if mf.save_result("typing/score.txt", result) :
+                print("The information has been saved")
+            else: print("Something went wrong!")
 
         elif u_choice == "3":
-            print(get_text("typing/hard.txt"))
-            result = show_lines(the_lines)
+            the_lines = mf.get_lines("typing/hard.txt")
+            result = mf.start_practice(the_lines)
             input("Press enter to see the result ...")
-            print("Feltecken:")
-            print(result)
+            mf.display_result(result)
+            result["name"] = str(input("\nEnter username to add to high score: "))
+            result["level"] = "hard"
+            if mf.save_result("typing/score.txt", result) :
+                print("The information has been saved")
+            else: print("Something went wrong!")
 
         elif u_choice == "4":
-            clearConsole()
-            show_result("typing/score.txt")
+            mf.clear_console()
+            mf.show_result("typing/score.txt")
 
         elif u_choice == "5":
-            clearConsole()
-            result = practice_random_char()
-            present_fail = (result['count_failed_character'] / result['count_total_character']) * 100
+            mf.clear_console()
+            result = mf.practice_random_char()
+            present_fail = ((result['count_failed_character'] /
+                             result['count_total_character']) * 100)
             print("Feltecken:")
             print(result['failed_letters'])
             print("Precentuellt fel: " + str(present_fail) + "%")
-            print("Gross WPM: ", result['gross_WPM'])
-            print("Net WPM: ", result['net_WPM'])
+            print("Gross WPM: ", result['gross_wpm'])
+            print("Net WPM: ", result['net_wpm'])
         else:
             print("The choice is not in the menu.")
 
         if is_running:
             print(2*"\n")
-            input(f"please press the enter to go back to the menu.")
+            input("please press the enter to go back to the menu.")
             print(2*"\n")
 
 
